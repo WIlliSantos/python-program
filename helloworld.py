48eab2ba2a33f30680b8842399a3562ab474031f
willi <willi.santos@infoprice.co>
2019-04-16 11:44:51 -0300

    Add ! to program

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 ### Comeco do codigo helloworld.py #############
 ### Programa que escreve na tela "Hello World" #
 
-print "Hello World"
+print "Hello World!"
 
 ### Fim do Programa ############################
